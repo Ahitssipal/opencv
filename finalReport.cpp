0daf3c0e36cdc488811c5fe6530f2b82d0832dc5
김윤태 <156992951+Ahitssipal@users.noreply.github.com>
2024-06-05 13:56:59 +0900

    Update finalReport.cpp

--- finalReport.cpp
+++ finalReport.cpp
@@ -4,131 +4,135 @@
 using namespace std;
 using namespace cv;
 
-Mat based(500, 700, CV_8UC3, Scalar(255, 255, 255));
+Mat based(506, 706, CV_8UC3, Scalar(255, 255, 255));
 Point ptOld;
 void on_mouse(int evnet, int x, int y, int flags, void* userdata);
 void edgeLine(Mat img);
 void drawText(Mat img, const string text);
 
-Rect rectDrawing(0, 0, 500, 500);
-Rect rectSave(500, 0, 200, 100);
-Rect rectLoad(500, 100, 200, 100);
-Rect rectClear(500, 200, 200, 100);
-Rect rectRun(500, 300, 200, 100);
-Rect rectExit(500, 400, 200, 100);
+Rect rectDrawing(0, 0, 506, 506);
+Rect drawingSpace(3, 3, 500, 500);
+Rect rectSave(506, 0, 200, 100);
+Rect rectLoad(506, 100, 200, 100);
+Rect rectClear(506, 200, 200, 100);
+Rect rectRun(506, 300, 200, 106);
+Rect rectExit(506, 406, 200, 100);
 
 int main(void)
 {
-	Mat numberDrawing(500, 500, CV_8UC3, Scalar(255, 255, 255));
-	edgeLine(numberDrawing);
-	numberDrawing.copyTo(based(rectDrawing));
+   Mat numberDrawing(506, 506, CV_8UC3, Scalar(255, 255, 255));
+   edgeLine(numberDrawing);
+   numberDrawing.copyTo(based(rectDrawing));
 
-	Mat numberSave(100, 200, CV_8UC3, Scalar(255, 255, 255));
-	edgeLine(numberSave);
-	drawText(numberSave, "Save");
-	numberSave.copyTo(based(rectSave));
+   Mat numberSave(100, 200, CV_8UC3, Scalar(255, 255, 255));
+   edgeLine(numberSave);
+   drawText(numberSave, "Save");
+   numberSave.copyTo(based(rectSave));
 
-	Mat numberLoad(100, 200, CV_8UC3, Scalar(255, 255, 255));
-	edgeLine(numberLoad);
-	drawText(numberLoad, "Load");
-	numberLoad.copyTo(based(rectLoad));
+   Mat numberLoad(100, 200, CV_8UC3, Scalar(255, 255, 255));
+   edgeLine(numberLoad);
+   drawText(numberLoad, "Load");
+   numberLoad.copyTo(based(rectLoad));
 
-	Mat numberClear(100, 200, CV_8UC3, Scalar(255, 255, 255));
-	edgeLine(numberClear);
-	drawText(numberClear, "Clear");
-	numberClear.copyTo(based(rectClear));
+   Mat numberClear(100, 200, CV_8UC3, Scalar(255, 255, 255));
+   edgeLine(numberClear);
+   drawText(numberClear, "Clear");
+   numberClear.copyTo(based(rectClear));
 
-	Mat numberRun(100, 200, CV_8UC3, Scalar(255, 255, 255));
-	edgeLine(numberRun);
-	drawText(numberRun, "Run");
-	numberRun.copyTo(based(rectRun));
+   Mat numberRun(106, 200, CV_8UC3, Scalar(255, 255, 255));
+   edgeLine(numberRun);
+   drawText(numberRun, "Run");
+   numberRun.copyTo(based(rectRun));
 
-	Mat numberExit(100, 200, CV_8UC3, Scalar(255, 255, 255));
-	edgeLine(numberExit);
-	drawText(numberExit, "Exit");
-	numberExit.copyTo(based(rectExit));
+   Mat numberExit(100, 200, CV_8UC3, Scalar(255, 255, 255));
+   edgeLine(numberExit);
+   drawText(numberExit, "Exit");
+   numberExit.copyTo(based(rectExit));
 
-	Mat clearBased = based.clone();
+   Mat clearBased = based.clone();
 
-	namedWindow("numberWindow");
-	setMouseCallback("numberWindow", on_mouse, &clearBased);
-	imshow("numberWindow", based);
-	waitKey(0);
+   namedWindow("numberWindow");
+   setMouseCallback("numberWindow", on_mouse, &clearBased);
+   imshow("numberWindow", based);
+   waitKey(0);
 
-	return 0;
+   return 0;
 }
 
 void on_mouse(int event, int x, int y, int flags, void* userdata)
 {
-	Mat* clearBased = static_cast<Mat*>(userdata);
-	switch (event)
-	{
-	case EVENT_LBUTTONDOWN:
-		if (rectDrawing.contains(Point(x,y))) {
-			ptOld = Point(x, y);
-		}
-		else if (rectSave.contains(Point(x, y)))
-		{
-			string filename;
-			cout << "ÀúÀåÇÒ ÆÄÀÏ¸í ÀÔ·Â : ";
-			getline(cin, filename);
-			bool saving = imwrite(filename, based(rectDrawing));
-			if (saving) cout << filename << " ÆÄÀÏÀÌ ÀúÀåµÊ." << endl;
-			else cout << filename << " ÆÄÀÏ ÀúÀå½ÇÆÐ." << endl;
-		}
-		else if (rectLoad.contains(Point(x, y)))
-		{
-			string loadFileName;
-			cout << "ºÒ·¯¿Ã ÆÄÀÏ¸íÀ» ÀÔ·Â : ";
-			getline(cin, loadFileName);
-			Mat temp = imread(loadFileName, IMREAD_COLOR);
-			if (temp.empty()) cout << loadFileName << " ºÒ·¯¿À±â ½ÇÆÐ." << endl;
-			else cout << loadFileName << " ºÒ·¯¿È." << endl;
-			temp.copyTo(based(rectDrawing));
-			imshow("numberWindow", based);
-		}
-		else if (rectClear.contains(Point(x, y)))
-		{
-			clearBased->copyTo(based);
-			imshow("numberWindow", based);
-			cout << "ÀÔ·ÂÃ¢ »èÁ¦µÊ." << endl;
-		}
-		else if (rectExit.contains(Point(x, y)))
-		{
-			cout << "ÇÁ·Î±×·¥ Á¾·á" << endl;
-			destroyAllWindows();
-		}
-		break;
-	case EVENT_MOUSEMOVE:
-		if (flags && EVENT_FLAG_LBUTTON && rectDrawing.contains(Point(x, y))) {
-			line(based, ptOld, Point(x, y), Scalar(0, 0, 0), 20);
-			imshow("numberWindow", based);
-		}
-		ptOld = Point(x, y);
-		break;
-	default:
-		break;
-	}
+   Mat* clearBased = static_cast<Mat*>(userdata);
+   switch (event)
+   {
+   case EVENT_LBUTTONDOWN:
+      if (drawingSpace.contains(Point(x, y))) {
+         ptOld = Point(x, y);
+      }
+      else if (rectSave.contains(Point(x, y)))
+      {
+         string filename;
+         cout << "ì €ìž¥í•  íŒŒì¼ëª… ìž…ë ¥ : ";
+         getline(cin, filename);
+         bool saving = imwrite(filename, based(drawingSpace));
+         if (saving) cout << filename << " íŒŒì¼ì´ ì €ìž¥ë¨." << endl;
+         else cout << filename << " íŒŒì¼ ì €ìž¥ì‹¤íŒ¨." << endl;
+      }
+      else if (rectLoad.contains(Point(x, y)))
+      {
+         string loadFileName;
+         cout << "ë¶ˆëŸ¬ì˜¬ íŒŒì¼ëª…ì„ ìž…ë ¥ : ";
+         getline(cin, loadFileName);
+         Mat temp = imread(loadFileName, IMREAD_COLOR);
+         if (temp.empty()) cout << loadFileName << " ë¶ˆëŸ¬ì˜¤ê¸° ì‹¤íŒ¨." << endl;
+         else cout << loadFileName << " ë¶ˆëŸ¬ì˜´." << endl;
+         temp.copyTo(based(drawingSpace));
+         imshow("numberWindow", based);
+      }
+      else if (rectClear.contains(Point(x, y)))
+      {
+         clearBased->copyTo(based);
+         imshow("numberWindow", based);
+         cout << "ìž…ë ¥ì°½ ì‚­ì œë¨." << endl;
+      }
+      else if (rectExit.contains(Point(x, y)))
+      {
+         cout << "í”„ë¡œê·¸ëž¨ ì¢…ë£Œ" << endl;
+         destroyAllWindows();
+      }
+      break;
+   case EVENT_MOUSEMOVE:
+      if (flags && EVENT_FLAG_LBUTTON && drawingSpace.contains(Point(x, y))) {
+         if (drawingSpace.contains(ptOld))
+         {
+            line(based, ptOld, Point(x, y), Scalar(0, 0, 0), 20);
+            imshow("numberWindow", based);
+         }
+      }
+      ptOld = Point(x, y);
+      break;
+   default:
+      break;
+   }
 }
 
 
 void edgeLine(Mat img)
 {
-	line(img, Point(0, 0), Point(img.cols - 1, 0), Scalar(0, 0, 0), 3);
-	line(img, Point(img.cols - 1, 0), Point(img.cols - 1, img.rows - 1), Scalar(0, 0, 0), 3);
-	line(img, Point(img.cols - 1, img.rows - 1), Point(0, img.rows - 1), Scalar(0, 0, 0), 3);
-	line(img, Point(0, img.rows - 1), Point(0, 0), Scalar(0, 0, 0), 3);
+   line(img, Point(0, 0), Point(img.cols - 1, 0), Scalar(0, 0, 0), 3);
+   line(img, Point(img.cols - 1, 0), Point(img.cols - 1, img.rows - 1), Scalar(0, 0, 0), 3);
+   line(img, Point(img.cols - 1, img.rows - 1), Point(0, img.rows - 1), Scalar(0, 0, 0), 3);
+   line(img, Point(0, img.rows - 1), Point(0, 0), Scalar(0, 0, 0), 3);
 }
 
 void drawText(Mat img, const string text)
 {
-	int fontFace = FONT_HERSHEY_SIMPLEX;
-	double fontScale = 1.5;
-	int thickness = 3;
+   int fontFace = FONT_HERSHEY_SIMPLEX;
+   double fontScale = 1.5;
+   int thickness = 3;
 
-	Size sizeText = getTextSize(text, fontFace, fontScale, thickness, 0);
-	Size sizeImg = img.size();
+   Size sizeText = getTextSize(text, fontFace, fontScale, thickness, 0);
+   Size sizeImg = img.size();
 
-	Point org((sizeImg.width - sizeText.width) / 2, (sizeImg.height + sizeText.height) / 2);
-	putText(img, text, org, fontFace, fontScale, Scalar(0, 0, 0), thickness);
+   Point org((sizeImg.width - sizeText.width) / 2, (sizeImg.height + sizeText.height) / 2);
+   putText(img, text, org, fontFace, fontScale, Scalar(0, 0, 0), thickness);
 }
